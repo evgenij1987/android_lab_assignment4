7bb904ec5ed6058f9fa61e9a3d2350739fdb862e
Evgenij <avstein@hotmail.de>
2015-05-29 23:25:35 +0200

    child process sdtout

--- plugserver/plugs.controller.js
+++ plugserver/plugs.controller.js
@@ -89,8 +89,8 @@ function runRadioTransmitter(house_code, switch_code, action){
 
     var exec = require('child_process').exec;
 
-    //var command="sudo ./../rspimodulator/rspimodulator ";//+house_code+switch_code+action;
-    var command="echo ./../rspimodulator/rspimodulator ";//+house_code+switch_code+action;
+    var command="sudo ./../rspimodulator/rspimodulator ";//+house_code+switch_code+action;
+   // var command="echo ./../rspimodulator/rspimodulator ";//+house_code+switch_code+action;
 
     exec(command, function (error, stdout, stderr) {
 
