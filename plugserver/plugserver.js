24a441f5fcd32455ffe54d595aca93c9eb3b02a2
evgenij1987 <avstein@hotmail.de>
2015-05-29 18:31:39 +0200

    comments

--- plugserver/plugserver.js
+++ plugserver/plugserver.js
@@ -4,13 +4,20 @@
 var express = require('express');
 var app = express();
 var PORT=3000;
-plugController=require('./plugs.controller');
 
+//controller switching on plugs and holding state
+plugController=require('./plugs.controller');
 plugController.init();//reading all available plugs from config.yaml
 
+//API to list all available plugs
 app.get('/api/plugs',plugController.sendPlugList);
+
+//API to turn on a plug by id served from /api/plugs e.g /api/plugs/turnOn/1
 app.get('/api/plugs/turnON/:id', plugController.turnOnPlug);
+//API to turn off a plug by id served from /api/plugs
 app.get('/api/plugs/turnOFF/:id', plugController.turnOffPlug);
+
 app.listen(PORT);
+
 console.log("plugserver running on: "+PORT);
 
